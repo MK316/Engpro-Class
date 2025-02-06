addf8086c034354b88bc514621e64b0f83546936
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 22:37:49 +0900

    Update 📕__Part_II_English_Vowels.py

--- pages/📕__Part_II_English_Vowels.py
+++ pages/📕__Part_II_English_Vowels.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
+# Create four tabs
+tabs = st.tabs(["💧 Contents", "💧 App1", "💧 App2", "💧 App3"])
 
-
+# Content for each tab
+with tabs[0]:
+    st.markdown("### 📒 ")
+    st.write("""
+    1. Lesson 1: Pronouncing English vowels
+    """
+with tabs[1]:
+    st.markdown("### 📒 Lesson 2: Tense and lax ‘i’ - sheep vs. ship")
+with tabs[2]:
+    st.markdown("### 📒 Lesson 3: Tense and lax ‘u’ - pool vs. pull")
+with tabs[3]:
+    st.markdown("### 📒 Lesson 4: Vowel pair in ‘bed’ and ‘bad’")
