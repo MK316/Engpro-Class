3030dc029e251b741779ca29efc6b9b737b5bc64
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 16:28:43 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
 
 st.caption("English Pronunciation Practice")
 st.title("Coming Soon")
 
 # Add an image from GitHub
 image_url = "https://github.com/MK316/Engpro-Class/raw/main/data/engpro-cover.png"
-st.image(image_url, caption="Got confidence", use_column_width=True)
+
+# Load and resize the image
+response = requests.get(image_url)
+img = Image.open(BytesIO(response.content))
+img = img.resize((250, 300))  # Set custom width and height
+
+st.image(img, caption="Practice Makes Perfect")
