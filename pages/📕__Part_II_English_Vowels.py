28cefde1d4988d6e1fc6f76bdaa2ecac8dee46c9
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 22:40:46 +0900

    Update 📕__Part_II_English_Vowels.py

--- pages/📕__Part_II_English_Vowels.py
+++ pages/📕__Part_II_English_Vowels.py
@@ -6,9 +6,18 @@ tabs = st.tabs(["💧 Contents", "💧 App1", "💧 App2", "💧 App3"])
 # Content for each tab
 with tabs[0]:
     st.markdown("### 📒 ")
-    st.write("""
-    1. Lesson 1: Pronouncing English vowels
-    """
+    st.markdown("""
+    1. **Lesson 1**: Pronouncing English vowels  
+    2. **Lesson 2**: Stress and rhythm in English  
+    3. **Lesson 3**: Consonant articulation  
+    4. **Lesson 4**: Connected speech patterns  
+    5. **Lesson 5**: Vowel [ɑ] and spelling confusion
+    6. **Lesson 6**: Vowels in ‘but’, ‘bought’, ‘boat’
+    7. **Lesson 7**: Diphthong vowels in English
+    8. **Lesson 8**: Unstressed vowel (schwa vowel) as in ‘ago’, ‘upon’, ‘company’
+    9. **Lesson 9**: R-colored vowels as in ‘perfect’ and ‘percent’
+
+    """)
 with tabs[1]:
     st.markdown("### 📒 Lesson 2: Tense and lax ‘i’ - sheep vs. ship")
 with tabs[2]:
