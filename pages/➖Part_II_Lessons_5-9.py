cd70c641d29650e2108dca8964536d4e8216af00
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 19:15:24 +0900

    Update ➖Part_II_Lessons_5-9.py

--- pages/➖Part_II_Lessons_5-9.py
+++ pages/➖Part_II_Lessons_5-9.py
@@ -5,13 +5,13 @@ tabs = st.tabs(["💧 Lesson 5", "💧 Lesson 6", "💧 Lesson 7", "💧 Lesson 
 
 # Content for each tab
 with tabs[0]:
-    st.markdown("### Lesson 5: Vowel [ɑ] and spelling confusion")
+    st.markdown("### 📒 Lesson 5: Vowel [ɑ] and spelling confusion")
 with tabs[1]:
-    st.markdown("### Lesson 6: Vowels in ‘but’, ‘bought’, ‘boat’")
+    st.markdown("### 📒 Lesson 6: Vowels in ‘but’, ‘bought’, ‘boat’")
 with tabs[2]:
-    st.markdown("### Lesson 7: Diphthong vowels in English")
+    st.markdown("### 📒 Lesson 7: Diphthong vowels in English")
 with tabs[3]:
-    st.markdown("### Lesson 8: Unstressed vowel (schwa vowel) as in ‘ago’, ‘upon’, ‘company’")
+    st.markdown("### 📒 Lesson 8: Unstressed vowel (schwa vowel) as in ‘ago’, ‘upon’, ‘company’")
 with tabs[4]:
-    st.markdown("### Lesson 9: R-colored vowels as in ‘perfect’ and ‘percent’")
+    st.markdown("### 📒 Lesson 9: R-colored vowels as in ‘perfect’ and ‘percent’")
 
