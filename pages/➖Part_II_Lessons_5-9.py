46951d9dd2f3ac9edcbbcfff9a8ad89afc79d952
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 19:12:22 +0900

    Update ➖Part_II_Lessons_5-9.py

--- pages/➖Part_II_Lessons_5-9.py
+++ pages/➖Part_II_Lessons_5-9.py
@@ -3,15 +3,13 @@ tabs = st.tabs(["💧 Lesson 5", "💧 Lesson 6", "💧 Lesson 7", "💧 Lesson 
 
 # Content for each tab
 with tabs[0]:
-    st.markdown("### Pronouncing English vowels")
-with tabs[1]:
-    st.markdown("### Tense and lax ‘i’ - sheep vs. ship")
-with tabs[2]:
-    st.markdown("### Tense and lax ‘u’ - pool vs. pull")
-with tabs[3]:
-    st.markdown("### Vowel pair in ‘bed’ and ‘bad’")
-with tabs[4]:
     st.markdown("### Vowel [ɑ] and spelling confusion")
-with tabs[5]:
+with tabs[1]:
     st.markdown("### Vowels in ‘but’, ‘bought’, ‘boat’")
+with tabs[2]:
+    st.markdown("### Diphthong vowels in English")
+with tabs[3]:
+    st.markdown("### Unstressed vowel (schwa vowel) as in ‘ago’, ‘upon’, ‘company’")
+with tabs[4]:
+    st.markdown("### R-colored vowels as in ‘perfect’ and ‘percent’")
 
