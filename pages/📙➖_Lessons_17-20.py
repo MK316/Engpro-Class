b59269e9d66475a7ceb4be9424f1f4de178532a1
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 22:32:26 +0900

    Update 📙➖_Lessons_17-20.py

--- pages/📙➖_Lessons_17-20.py
+++ pages/📙➖_Lessons_17-20.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+# Create four tabs
+tabs = st.tabs(["💧 Lesson 17", "💧 Lesson 18", "💧 Lesson 19", "💧 Lesson 20"])
+
+# Content for each tab
+with tabs[0]:
+    st.markdown("### 📒 Lesson 17: ")
+with tabs[1]:
+    st.markdown("### 📒 Lesson 18: ")
+with tabs[2]:
+    st.markdown("### 📒 Lesson 19: ")
+with tabs[3]:
+    st.markdown("### 📒 Lesson 20: ")
