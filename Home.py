d4e5f8fc14d02ea024ad8e510a388105ed069a55
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 16:26:34 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,5 +4,5 @@ st.caption("English Pronunciation Practice")
 st.title("Coming Soon")
 
 # Add an image from GitHub
-image_url = "https://raw.githubusercontent.com/MK316/Engpro-Class/data/engpro-cover.png"
+image_url = "https://github.com/MK316/Engpro-Class/raw/main/data/engpro-cover.png"
 st.image(image_url, caption="Got confidence", use_column_width=True)
