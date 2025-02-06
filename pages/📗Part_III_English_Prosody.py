880475493d3dedf66e7b57292f61cecc26b9a241
MK <99416359+MK316@users.noreply.github.com>
2025-02-06 22:42:01 +0900

    Update 📗Part_III_English_Prosody.py

--- pages/📗Part_III_English_Prosody.py
+++ pages/📗Part_III_English_Prosody.py
@@ -1,1 +1,22 @@
 import streamlit as st
+
+# Create four tabs
+tabs = st.tabs(["💧 Contents", "💧 App1", "💧 App2", "💧 App3"])
+
+# Content for each tab
+with tabs[0]:
+    st.markdown("### 📒 ")
+    st.markdown("""
+    1. **Lesson 10**: Pronouncing English vowels  
+    2. **Lesson 11**: Stress and rhythm in English  
+    3. **Lesson 12**: Consonant articulation  
+    4. **Lesson 13**: Connected speech patterns  
+    5. **Lesson 14**: Vowel [ɑ] and spelling confusion
+
+    """)
+with tabs[1]:
+    st.markdown("### 📒 Lesson 2: Tense and lax ‘i’ - sheep vs. ship")
+with tabs[2]:
+    st.markdown("### 📒 Lesson 3: Tense and lax ‘u’ - pool vs. pull")
+with tabs[3]:
+    st.markdown("### 📒 Lesson 4: Vowel pair in ‘bed’ and ‘bad’")
