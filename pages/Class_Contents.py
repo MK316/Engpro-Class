bdaddb1d96c5bc6b9a7e3edca970658a8bfcd931
MK <99416359+MK316@users.noreply.github.com>
2025-02-07 14:59:57 +0900

    Update Class_Contents.py

--- pages/Class_Contents.py
+++ pages/Class_Contents.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("Class content overview")
+st.title("Course overview")
+st.caption("In this class, we use a workbook compiled from handouts as the textbook.")
 
 st.write("The English Pronunciation course is designed for first-year English Education majors and aims to train future teachers who will learn and teach English as a foreign language. It focuses on making them aware of the role and importance of pronunciation in communication and developing their ability to train themselves by acquiring phonetic knowledge. To this end, the course uses a workbook that allows for systematic learning of practical knowledge rather than theoretical content, including the individual sounds, intonation, and rhythm of English.")
 st.write("The content covered in the course is divided into four parts, each focusing on the following topics as the course progresses:")
