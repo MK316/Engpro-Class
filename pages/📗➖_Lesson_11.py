75ac4d5057e8e5d7848109ba73c9290c556ef2f1
MK <99416359+MK316@users.noreply.github.com>
2025-02-09 01:02:54 +0900

    Update 📗➖_Lesson_11.py

--- pages/📗➖_Lesson_11.py
+++ pages/📗➖_Lesson_11.py
@@ -5,7 +5,17 @@ tabs = st.tabs(["📗 Lesson 11", "❄️ App1", "❄️ App2", "❄️ App3"])
 
 # Content for each tab
 with tabs[0]:
-    st.markdown("### 📒 Lesson 11: ")
+    st.markdown("### 📒 Lesson 11: Accents in English")
+    st.write("In addition to word stress, which is assigned to individual syllables, English words can also receive accents within a sentence. This combination of stress and accent leads to greater emphasis. For instance, stressed syllables are naturally more emphasized than unstressed ones. Moreover, stressed syllables in accented words are even stronger and longer than those in unaccented words.")
+    st.write("Example) My **younger** brother ate **ten** chocolate **cookies.**")
+
+
+    st.markdown("##### 1. New information is usually accented with a high pitch")
+
+    st.write("My name is Jane Smith. (H* vs. L* carry different meanings)")
+    st.markdown("##### 2. The final key word within a sentence receives the most emphasis in English.")
+    
+
 with tabs[1]:
     st.markdown("### 📒 Lesson 15: ")
 with tabs[2]:
