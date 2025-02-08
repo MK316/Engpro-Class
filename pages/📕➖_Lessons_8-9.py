d6c70d252415ab99d85cb4a55b309547dcf3b8a1
MK <99416359+MK316@users.noreply.github.com>
2025-02-08 16:58:05 +0900

    Update 📕➖_Lessons_8-9.py

--- pages/📕➖_Lessons_8-9.py
+++ pages/📕➖_Lessons_8-9.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+tabs = st.tabs("Lesson8","Lesson9","Listening")
+
+with tabs[0]:
+    st.markdown("### 📒 Lesson 8: Unstressed vowel (schwa vowel) as in ‘ago’, ‘upon’, ‘company’")
+with tabs[1]:
+    st.markdown("### 📒 Lesson 9: R-colored vowels as in ‘perfect’ and ‘percent’")
+with tabs[2]:
+    st.markdown("""
+    -Lesson 8
+    -Lesson 9
+    """)
+  
