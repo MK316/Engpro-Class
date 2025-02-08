68a85ef14965aa21d873833952b2667e632f6c08
MK <99416359+MK316@users.noreply.github.com>
2025-02-08 16:56:31 +0900

    Update and rename 📕➖_Lessons_5-9.py to 📕➖_Lessons_5-7.py

---
+++ pages/📕➖_Lessons_5-7.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Create four tabs
+tabs = st.tabs(["💧 Lesson 5", "💧 Lesson 6", "💧 Lesson 7", "Listening"])
+
+# Content for each tab
+with tabs[0]:
+    st.markdown("### 📒 Lesson 5: Vowel [ɑ] and spelling confusion")
+with tabs[1]:
+    st.markdown("### 📒 Lesson 6: Vowels in ‘but’, ‘bought’, ‘boat’")
+with tabs[2]:
+    st.markdown("### 📒 Lesson 7: Diphthong vowels in English")
+with tabs[3]"
+    st.markdown("""
+    - Lesson 5
+    - Lesson 6
+    - Lesson 7
+    """)
+
