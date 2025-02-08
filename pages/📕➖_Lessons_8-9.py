1a1e65f967384f831a2c5c772ec4c981e26a2782
MK <99416359+MK316@users.noreply.github.com>
2025-02-08 20:25:39 +0900

    Update 📕➖_Lessons_8-9.py

--- pages/📕➖_Lessons_8-9.py
+++ pages/📕➖_Lessons_8-9.py
@@ -3,6 +3,7 @@ from gtts import gTTS
 import io
 import time
 from pydub import AudioSegment
+from pydub.playback import play
 
 # Create tabs
 tabs = st.tabs(["Lesson8", "Lesson9", "Listening"])
@@ -59,15 +60,16 @@ with tabs[0]:
         else:
             st.write("🎵 Strong - Weak | Strong - Weak | Strong - Weak")
             time.sleep(0.5)
-            # Generate melody beats and play in Streamlit
+            # Generate melody beats
             mi = AudioSegment.sine(frequency=659, duration=1000)
             do = AudioSegment.sine(frequency=523, duration=300)
             beat_audio = mi + do
             full_audio = beat_audio * 3
+            # Export and play the melody in Streamlit
             audio_data = io.BytesIO()
             full_audio.export(audio_data, format="mp3")
             audio_data.seek(0)
-            st.audio(audio_data, format='audio/mp3')
+            st.audio(audio_data.getvalue(), format='audio/mp3')
 
 # Placeholder for Lesson 9
 with tabs[1]:
