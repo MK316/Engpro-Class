859b2e4d22238d8bba623b42bbdec1ebfafe04e6
MK <99416359+MK316@users.noreply.github.com>
2025-02-08 17:24:23 +0900

    Update 📕➖_Lessons_5-7.py

--- pages/📕➖_Lessons_5-7.py
+++ pages/📕➖_Lessons_5-7.py
@@ -6,11 +6,6 @@ tabs = st.tabs(["💧 Lesson 5", "💧 Lesson 6", "💧 Lesson 7", "Listening"])
 # Content for each tab
 with tabs[0]:
     st.markdown("### 📒 Lesson 5: Vowel [ɑ] and spelling confusion")
-
-
-
-with tabs[1]:
-    st.markdown("### 📒 Lesson 6: Vowels in ‘but’, ‘bought’, ‘boat’")
      # Using columns to place images side-by-side
     col1, col2 = st.columns(2)
     with col1:
@@ -19,6 +14,11 @@ with tabs[1]:
     with col2:
         st.image("https://github.com/MK316/Engpro-Class/blob/main/images/bigA.jpg",
                  width=300, caption="[ɑ]")
+
+
+with tabs[1]:
+    st.markdown("### 📒 Lesson 6: Vowels in ‘but’, ‘bought’, ‘boat’")
+
 with tabs[2]:
     st.markdown("### 📒 Lesson 7: Diphthong vowels in English")
 with tabs[3]:
