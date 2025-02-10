aec26c6227ae3530965853afd3aa6bf33fe30b7b
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 02:38:36 +0900

    Update Bookmarks_&_Apps.py

--- pages/Bookmarks_&_Apps.py
+++ pages/Bookmarks_&_Apps.py
@@ -92,7 +92,7 @@ def main():
         # Creating clickable markdown buttons
         st.markdown('<a href="https://mk-316-accuracyfeedback.hf.space" class="button_link" target="_blank">🍋 App 1: Accuracy Feedback</a>', unsafe_allow_html=True)
         st.markdown('<a href="https://mk-316-tts-pitch.hf.space" class="button_link" target="_blank">🍐 App 2: Intonation contour</a>', unsafe_allow_html=True)
-        st.markdown('<a href="https://mk-316-foreignaccent.hf.space" class="button_link" target="_blank">🐬 App 3: Foreign accent examples contour</a>', unsafe_allow_html=True)
+        st.markdown('<a href="https://mk-316-foreignaccent.hf.space" class="button_link" target="_blank">🐼 App 3: Foreign accent examples contour</a>', unsafe_allow_html=True)
         st.markdown('<a href="https://https://mk-316-pronunciationfeedback.hf.space/" class="button_link" target="_blank">📝 App 4: Pronunciation Feedback</a>', unsafe_allow_html=True)
         st.markdown('<a href="https://mk-316-korean-english.hf.space" class="button_link" target="_blank">🎧 App 5: Loanword English Pronunciation</a>', unsafe_allow_html=True)
         st.markdown('<a href="https://mk-316-oxford5k.hf.space" class="button_link" target="_blank">🐇 App 6: Oxford 5K Vocabulary practice</a>', unsafe_allow_html=True)
