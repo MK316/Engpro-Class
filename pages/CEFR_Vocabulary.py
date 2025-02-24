3df7e0a973e00e4fdf5b3b45826c6e24bc26f9e8
MK <99416359+MK316@users.noreply.github.com>
2025-02-24 19:21:59 +0900

    Update CEFR_Vocabulary.py

--- pages/CEFR_Vocabulary.py
+++ pages/CEFR_Vocabulary.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
 st.markdown("""
-    #### CEFR B1B2 and C1 wordlist
-    Click the link below to visit the vocabulary practice application:
-    
-  '<a href="https://mk316voca.streamlit.app/" target="_blank">🔗 Open Listen and Spell</a>',
-    unsafe_allow_html=True
-""")
+    #### CEFR B1B2 and C1 Wordlist  
+    Click the link below to visit the vocabulary practice application:  
 
+    <a href="https://mk316voca.streamlit.app/" target="_blank">🔗 Open Listen and Spell</a>
+""", unsafe_allow_html=True)
