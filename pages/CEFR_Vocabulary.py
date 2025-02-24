7d2662c1fc38081a5be8cd98e0abd15aae8a063e
MK <99416359+MK316@users.noreply.github.com>
2025-02-24 19:19:52 +0900

    Update CEFR_Vocabulary.py

--- pages/CEFR_Vocabulary.py
+++ pages/CEFR_Vocabulary.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.markdown(
-    '<a href="https://mk316voca.streamlit.app/" target="_blank">🔗 Open Listen and Spell</a>',
+st.markdown("""
+    ### '<a href="https://mk316voca.streamlit.app/" target="_blank">🔗 Open Listen and Spell</a>',
     unsafe_allow_html=True
-)
+""")
 
