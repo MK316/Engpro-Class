6372447188edbe8b0faa6db803f9c6adbb927ea8
MK <99416359+MK316@users.noreply.github.com>
2025-02-26 13:50:14 +0900

    Create Q&As.py

---
+++ pages/Q&As.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("🐾 Files to share: on Padlet")
+st.write("This Padlet serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
+st.components.v1.iframe("https://padlet.com/mirankim316/QNA", width=700, height=800)
