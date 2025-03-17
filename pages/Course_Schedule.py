3ce4b8154e00ae3a4c2df7ff26dbcab0fc0c8f52
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 01:35:05 +0900

    Create Course_Schedule.py

---
+++ pages/Course_Schedule.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+tabs = st.tabs(["💧 Schedule", "💧 Hey Jude", 
+
+
+##############################
+# Content for the Schedule tab
+with tabs[0]:
+    st.caption("Spring 2025")
+    # URL of the raw markdown file on GitHub
+    markdown_url = "https://raw.githubusercontent.com/MK316/Engpro-Class/refs/heads/main/pages/readme.md"
+    
+    try:
+        response = requests.get(markdown_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        markdown_content = response.text
+        st.markdown(markdown_content, unsafe_allow_html=True)
+    except requests.exceptions.HTTPError as err:
+        st.error(f"Failed to retrieve Markdown content: {err}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request failed: {e}")
+
+with tabs[1]:
+    st.markdown("### 🎬 Hey Jude (HW #4)")
+
+    # Define the URLs for the YouTube videos
+    st.markdown("#### 1. Hey Jude (Beatles)")
+    video_url1 = "https://www.youtube.com/embed/mQER0A0ej0M?si=eSi7vtIJzot9cqAf"
+    st.video(video_url1, format="video/mp4", start_time=0)
+
+    st.markdown("---")
+    st.write("Lyrics")
+    st.markdown("""
+    **Lyrics**
+    
+    Hey Jude, don't make it bad.
+    Take a sad song and make it better.
+    Remember to let her into your heart,
+    Then you can start to make it better.  
+    
+    Hey Jude, don't be afraid.
+    You were made to go out and get her.
+    The minute you let her under your skin,
+    Then you begin to make it better.  
+    
+    And anytime you feel the pain, hey Jude, refrain,
+    Don't carry the world upon your shoulders.
+    For well you know that it's a fool who plays it cool
+    By making his world a little colder.  
+    
+    Hey Jude, don't let me down.
+    You have found her, now go and get her.
+    Remember to let her into your heart,
+    Then you can start to make it better.  
+    
+    So let it out and let it in, hey Jude, begin,
+    You're waiting for someone to perform with.
+    And don't you know that it's just you, hey Jude, you'll do,
+    The movement you need is on your shoulder.  
+    
+    Hey Jude, don't make it bad.
+    Take a sad song and make it better.
+    Remember to let her under your skin,
+    Then you'll begin to make it
+    Better better better better better better, oh.  
+    
+    Na na na nananana, nannana, hey Jude...
+    (repeat X number of times, fade)
+
+    """)
+              
