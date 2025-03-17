0000835e4bf4f16b2c7a86b1f3d2482d1cff65f0
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 01:39:37 +0900

    Update About_the_Course_Schedule.py

--- pages/About_the_Course_Schedule.py
+++ pages/About_the_Course_Schedule.py
@@ -22,7 +22,7 @@ with tabs[0]:
         st.error(f"Request failed: {e}")
 
 with tabs[1]:
-    st.markdown("### 🎬 Hey Jude (HW #4)")
+    st.markdown("### 🎼  Hey Jude (HW #4)")
 
     # Define the URLs for the YouTube videos
     st.markdown("#### 1. Hey Jude (Beatles)")
@@ -31,33 +31,39 @@ with tabs[1]:
 
     st.markdown("---")
     st.markdown("""
-    **Lyrics**
-    
+    🎼 **Lyrics**
+
+    1.  
     Hey Jude, don't make it bad.  
     Take a sad song and make it better.  
     Remember to let her into your heart,  
     Then you can start to make it better.  
     
+    2.  
     Hey Jude, don't be afraid.  
     You were made to go out and get her.  
     The minute you let her under your skin,  
     Then you begin to make it better.  
-    
+
+    3.  
     And anytime you feel the pain, hey Jude, refrain,  
     Don't carry the world upon your shoulders.  
     For well you know that it's a fool who plays it cool  
     By making his world a little colder.  
-    
+
+    4.  
     Hey Jude, don't let me down.  
     You have found her, now go and get her.  
     Remember to let her into your heart,  
     Then you can start to make it better.  
     
+    5.  
     So let it out and let it in, hey Jude, begin,  
     You're waiting for someone to perform with.  
     And don't you know that it's just you, hey Jude, you'll do,  
     The movement you need is on your shoulder.  
     
+    6.  
     Hey Jude, don't make it bad.  
     Take a sad song and make it better.  
     Remember to let her under your skin,  
