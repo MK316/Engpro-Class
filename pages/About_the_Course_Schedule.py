4c46094074eaaed931e209ce72b7b1f7b57a488b
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 01:37:44 +0900

    Update About_the_Course_Schedule.py

--- pages/About_the_Course_Schedule.py
+++ pages/About_the_Course_Schedule.py
@@ -30,42 +30,41 @@ with tabs[1]:
     st.video(video_url1, format="video/mp4", start_time=0)
 
     st.markdown("---")
-    st.write("Lyrics")
     st.markdown("""
     **Lyrics**
     
-    Hey Jude, don't make it bad.
-    Take a sad song and make it better.
-    Remember to let her into your heart,
+    Hey Jude, don't make it bad.  
+    Take a sad song and make it better.  
+    Remember to let her into your heart,  
     Then you can start to make it better.  
     
-    Hey Jude, don't be afraid.
-    You were made to go out and get her.
-    The minute you let her under your skin,
+    Hey Jude, don't be afraid.  
+    You were made to go out and get her.  
+    The minute you let her under your skin,  
     Then you begin to make it better.  
     
-    And anytime you feel the pain, hey Jude, refrain,
-    Don't carry the world upon your shoulders.
-    For well you know that it's a fool who plays it cool
+    And anytime you feel the pain, hey Jude, refrain,  
+    Don't carry the world upon your shoulders.  
+    For well you know that it's a fool who plays it cool  
     By making his world a little colder.  
     
-    Hey Jude, don't let me down.
-    You have found her, now go and get her.
-    Remember to let her into your heart,
+    Hey Jude, don't let me down.  
+    You have found her, now go and get her.  
+    Remember to let her into your heart,  
     Then you can start to make it better.  
     
-    So let it out and let it in, hey Jude, begin,
-    You're waiting for someone to perform with.
-    And don't you know that it's just you, hey Jude, you'll do,
+    So let it out and let it in, hey Jude, begin,  
+    You're waiting for someone to perform with.  
+    And don't you know that it's just you, hey Jude, you'll do,  
     The movement you need is on your shoulder.  
     
-    Hey Jude, don't make it bad.
-    Take a sad song and make it better.
-    Remember to let her under your skin,
-    Then you'll begin to make it
+    Hey Jude, don't make it bad.  
+    Take a sad song and make it better.  
+    Remember to let her under your skin,  
+    Then you'll begin to make it  
     Better better better better better better, oh.  
     
-    Na na na nananana, nannana, hey Jude...
+    Na na na nananana, nannana, hey Jude...  
     (repeat X number of times, fade)
 
     """)
