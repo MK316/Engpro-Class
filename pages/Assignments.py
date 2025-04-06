468a448492582024c98dbe8246409784c2755e15
MK <99416359+MK316@users.noreply.github.com>
2025-04-06 20:04:55 +0900

    Update Assignments.py

--- pages/Assignments.py
+++ pages/Assignments.py
@@ -11,7 +11,7 @@ with tab1:
     # Sample data for the assignments table
     data = {
         "Assignment": ["HW#1: Pre-recording", "HW#2: TED selection", "HW#3: 1 min's video recording", "HW#4: Hey Jude", "HW#5: TBA"],
-        "Due Date": ["2025-03-09", "2025-03-18", "2025-04-22", "2025-04-10", "2025-04-15"],
+        "Due Date": ["2025-03-09", "2025-03-18", "2025-04-22", "2025-05-13", "2025-04-15"],
         "Status": ["Closed", "Closed", "Open", "TBA", "TBA"],
         "Grade": ["Complete/Incomplete", "C / I", "4 pts.", "4 pts.", "4 pts."],
         "Submission": ["[LMS](https://rec.ac.kr/gnu)", "[Google Sheet](https://docs.google.com/spreadsheets/d/1vi-wOJEFpXNWInfcKEZKqiuNFzOQtib5_1R3qyT6N9E/edit?usp=sharing)", "[Padlet](https://padlet.com/mirankim316/S25Engpro)", "[Padlet](https://padlet.com/mirankim316/S25Engpro)", "TBA"],
