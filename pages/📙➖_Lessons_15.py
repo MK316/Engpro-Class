9d60e48790261b0eadcbf1e0450ee06910088f40
MK <99416359+MK316@users.noreply.github.com>
2025-05-08 13:15:08 +0900

    Update 📙➖_Lessons_15.py

--- pages/📙➖_Lessons_15.py
+++ pages/📙➖_Lessons_15.py
@@ -1,2 +1,7 @@
 import streamlit as st
 
+
+
+image_url = "https://github.com/MK316/Engpro-Class/raw/main/images/L15-keywords.png"
+st.image(image_url, caption="A: B, C (Place), A, B: C (Manner)", use_column_width=True)
+
