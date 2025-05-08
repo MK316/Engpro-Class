cf7ee97cceba8d7eaac9f3c7de1fe2b525e1bf79
MK <99416359+MK316@users.noreply.github.com>
2025-05-08 13:17:37 +0900

    Update 📙➖_Lessons_15.py

--- pages/📙➖_Lessons_15.py
+++ pages/📙➖_Lessons_15.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("### Lesson 15. Three consonant pairs")
+st.markdown("### 📙 Lesson 15. Three consonant pairs")
 
 image_url = "https://github.com/MK316/Engpro-Class/raw/main/images/L15-keywords.png"
 st.image(image_url, caption="A: B, C (Place); A, B: C (Manner)", use_container_width=True)
