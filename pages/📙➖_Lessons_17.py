8ca6bf454bb2094d76d5d3567645d36eafeb3abe
MK <99416359+MK316@users.noreply.github.com>
2025-05-13 14:33:15 +0900

    Create 📙➖_Lessons_17.py

---
+++ pages/📙➖_Lessons_17.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+tab1, tab2, tab3 = st.tabs(["Lesson", "Past tense form -ed", "APP"])
+
+with tab1:
+  st.markdown("### Lesson 17. [p/b, t/d, k/g]
+  
+
+with tab2:
+  st.markdown("### Past tense form -ed pronunciaiton")
+  
+with tab3:
+  st.markdown("### TBA")
