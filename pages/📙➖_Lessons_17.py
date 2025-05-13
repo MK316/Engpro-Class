024e3e6ca02c6e1e3e7bc68926ea32f1da657969
MK <99416359+MK316@users.noreply.github.com>
2025-05-13 14:51:23 +0900

    Update 📙➖_Lessons_17.py

--- pages/📙➖_Lessons_17.py
+++ pages/📙➖_Lessons_17.py
@@ -5,29 +5,11 @@ tab1, tab2, tab3 = st.tabs(["Lesson", "Past tense form -ed", "APP"])
 with tab1:
     st.markdown("### 📙 Lesson 17. [p/b, t/d, k/g]")
 
-    st.markdown("""
-    #### [1] Individual Sounds
-
-    Pronouncing [p/b, t/d, k/g] sounds is usually not a problem for Korean speakers.  
-    However, English has specific consonant rules that affect how voiceless sounds behave:
-
-    - After the /s/ sound (e.g., **spy**, **skate**, **steam**), voiceless sounds may lose their aspiration.  
-    - In unstressed syllables, consonants can sound weaker or different than expected.
-
-    Compare the following word pairs:
-    """)
-
 
 
 with tab2:
     st.markdown("### 📘 Past Tense Form: -ed Pronunciation")
-    st.markdown("""
-    The past tense ending **-ed** can be pronounced in three different ways in English:
 
-    - /t/ as in **washed**
-    - /d/ as in **played**
-    - /ɪd/ as in **wanted**
-    """)
 
 with tab3:
     st.markdown("### ⚙️ Interactive App Coming Soon")
