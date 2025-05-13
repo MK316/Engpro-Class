6e33302c7b5085eac2df6d17efec9670a7fa05d6
MK <99416359+MK316@users.noreply.github.com>
2025-05-13 14:34:21 +0900

    Update 📙➖_Lessons_17.py

--- pages/📙➖_Lessons_17.py
+++ pages/📙➖_Lessons_17.py
@@ -3,7 +3,7 @@ import streamlit as st
 tab1, tab2, tab3 = st.tabs(["Lesson", "Past tense form -ed", "APP"])
 
 with tab1:
-  st.markdown("### Lesson 17. [p/b, t/d, k/g]
+  st.markdown("### Lesson 17. [p/b, t/d, k/g]")
   
 
 with tab2:
