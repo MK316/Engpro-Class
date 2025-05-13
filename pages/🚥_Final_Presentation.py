f4d3e62095610b986a91b6022ec8a4591b358f35
MK <99416359+MK316@users.noreply.github.com>
2025-05-13 10:48:09 +0900

    Update 🚥_Final_Presentation.py

--- pages/🚥_Final_Presentation.py
+++ pages/🚥_Final_Presentation.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.markdown("### 💦 Presentation Guidelines for Future English Teachers")
 st.caption("🐥 This presentation is not just about showing what you know — it is a chance to **train yourself as a future teacher**. Use it as a **simulation of real classroom teaching**.")
-           
+
+url = "https://github.com/MK316/Engpro-Class/blob/main/images/presenter.png"
+st.image(url, width=400)
+
 st.markdown("""
 
 ---
