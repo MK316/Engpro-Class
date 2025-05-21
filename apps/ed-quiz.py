d0fa5313f15a67e1d4e46a0c07a7289ae3bd7be6
MK <99416359+MK316@users.noreply.github.com>
2025-05-21 22:35:22 +0900

    Update ed-quiz.py

--- apps/ed-quiz.py
+++ apps/ed-quiz.py
@@ -157,8 +157,8 @@ user_choice = st.radio(
     key=f"choice_{st.session_state.trials}"
 )
 
-col1, col2 = st.columns([2, 2])
-
+#col1, col2 = st.columns([2, 2])
+spacer, col1, col2, spacer2 = st.columns([1, 2, 2, 1])
 
 if col1.button("✅ Check the Answer", key=f"check_{st.session_state.trials}"):
     user_raw = reverse_map[user_choice]
