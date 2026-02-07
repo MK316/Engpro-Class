42d31b14cdaf0d3b690b6a10b2817ec630951130
MK <99416359+MK316@users.noreply.github.com>
2026-02-07 23:54:25 +0900

    Update 🚥_Q&As.py

--- pages/🚥_Q&As.py
+++ pages/🚥_Q&As.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header("🐾 Q & As: on Padlet")
 st.write("This Padlet serves as a dynamic hub for Q & As regarding the course. Personal inquiries can be directred via E-mail at mirankim@gnu.ac.kr")
-st.components.v1.iframe("https://padlet.com/mirankim316/QNA", width=700, height=600)
+st.components.v1.iframe("https://padlet.com/mirankim316/s26engpro", width=700, height=600)
