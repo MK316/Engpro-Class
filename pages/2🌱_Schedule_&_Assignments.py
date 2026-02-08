833d4abc1b989c24272ad1be4c968a204c5c613d
MK <99416359+MK316@users.noreply.github.com>
2026-02-08 12:56:19 +0900

    Update 2🌱_Schedule_&_Assignments.py

--- pages/2🌱_Schedule_&_Assignments.py
+++ pages/2🌱_Schedule_&_Assignments.py
@@ -1,27 +1,12 @@
 import streamlit as st
 import requests
 
-tabs = st.tabs(["💧 Schedule", "💧 Hey Jude", "Todolist"])
+tabs = st.tabs(["💧 Hey Jude", "Todolist", "💧 Schedule"])
 
 
 ##############################
 # Content for the Schedule tab
 with tabs[0]:
-    st.caption("Spring 2025")
-    # URL of the raw markdown file on GitHub
-    markdown_url = "https://raw.githubusercontent.com/MK316/Engpro-Class/refs/heads/main/pages/readme.md"
-    
-    try:
-        response = requests.get(markdown_url)
-        response.raise_for_status()  # Raises an HTTPError for bad responses
-        markdown_content = response.text
-        st.markdown(markdown_content, unsafe_allow_html=True)
-    except requests.exceptions.HTTPError as err:
-        st.error(f"Failed to retrieve Markdown content: {err}")
-    except requests.exceptions.RequestException as e:
-        st.error(f"Request failed: {e}")
-
-with tabs[1]:
     st.markdown("### 🎼  Hey Jude (HW #4)")
 
     # Define the URLs for the YouTube videos
@@ -77,5 +62,8 @@ with tabs[1]:
 
     st.markdown("[samples on Padlet](https://padlet.com/mirankim316/S26Engpro)")
 
-with tabs[2]:
+with tabs[1]:
     st.markdown("To be announced")
+
+with tabs[2]:
+    st.write("To be updated")
